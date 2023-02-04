44cd9f7984da61c15ef6bbe783ecc5152bc96dcd
Никита <@HOME-PC>
2023-02-04 15:36:26 +0300

    Добавьте файлы проекта.

---
+++ rpg/Arrays.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace RpgLogicProject
+
+{
+
+    public class Arrays
+
+    {
+
+        public static string[] monsters = new string[10] { "Зомби", "Паук", "Скелет", "Тролль", "Летучая мышь", "Гигантская крыса", "Волк", "Дух", "Змея", "Варвар" };
+
+        public static string[] loot = new string[10] { "Клык", "Шкура", "Старинная монета", "Кожа", "Осколок вазы", "Кроличья лапка", "Крылья летучей мыши", "Маска Адепта Кальвы", "Шишка Мудрого Дерева", "Кусок шёлка" };
+
+    }
+
+}
---
+++ rpg/Program.cs
@@ -0,0 +1,104 @@
+﻿using RpgLogicProject;
+using System;
+
+
+
+//Инициализировать классы. И начать битву  
+
+var rand = new Random();
+
+Console.WriteLine("Добро пожаловать на арену!");
+
+Console.WriteLine("Ваше имя?");
+
+Console.Write("Меня зовут:");
+
+string name = Convert.ToString(Console.ReadLine());
+
+var Hero = new Hero(name, 1, 100);
+
+
+var Weapon = new Weapon("Нож", 11);
+
+Console.WriteLine($"Твоим оружием будет: {Weapon.Name}");
+
+var Enemy = new Enemy(Arrays.monsters[rand.Next(0, 10)], Hero.Level + rand.Next(0, 1), rand.Next(0, 100), rand.Next(0, 50));
+
+Console.WriteLine($"Твой враг: {Enemy.Name}");
+
+
+Console.WriteLine("Готов?");
+
+Console.WriteLine("1-Да/2-Нет");
+
+int answer = int.Parse(Console.ReadLine());
+
+Console.WriteLine("------------------------------------");
+
+if (answer == 1)
+
+{
+
+
+
+    Console.WriteLine("Да начнётся бой!");
+
+    var BattleArena = new BattleArena(Enemy, Hero, Weapon);
+
+    var BackPack = new BackPack(10);
+
+    BackPack.Looting(BattleArena.Battle());
+
+}
+
+else if (answer == 2)
+
+{
+
+    Console.WriteLine("Уходишь?");
+
+    Console.WriteLine("Вот тебе два варианта:");
+
+    Console.WriteLine("1-Уйти,2-Выйти на поле боя.");
+
+res:
+
+    Console.WriteLine("Я выбираю:");
+
+    int answer1 = int.Parse(Console.ReadLine());
+
+    if (answer1 == 1)
+
+    {
+
+        Console.WriteLine($"{Hero.Name} У тебя был шанс стать великим бойцом");
+
+        Console.WriteLine("------------------------------------");
+
+        Console.WriteLine("Игра окончена...");
+
+    }
+
+    if (answer1 == 2)
+
+    {
+
+        Console.WriteLine($"Хорошо {Hero.Name}");
+
+        Console.WriteLine("Бой начинается прямо сейчас!");
+
+        var BattleArena = new BattleArena(Enemy, Hero, Weapon);
+
+        var BackPack = new BackPack(10);
+
+        BackPack.Looting(BattleArena.Battle());
+
+
+
+    }
+
+
+}
+
+
+
